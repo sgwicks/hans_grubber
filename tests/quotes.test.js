7f2a6ecf1b1eaf8dd4783da06aa1e3d885b47e27
sgwicks <samgwicks@gmail.com>
2020-09-26 21:29:21 +0100

    Add tests to do

--- tests/quotes.test.js
+++ tests/quotes.test.js
@@ -18,4 +18,41 @@ describe('callQuote', () => {
 
     expect(callQuote(msg)).toBeDefined();
   });
+  test.todo('Given a number, responds with a specific quote');
+  test.todo('If no quote exists for number, returns a message');
+  test.todo('Returns a quote includes a given string');
+  test.todo('If no quote exists matching string, returns a message');
+  test.todo('Appends game where it exists');
+  test.todo('Increments call count');
+});
+
+xdescribe('addQuote', () => {
+  test.todo('Adds quote to database');
+  test.todo('Adds game to database when given');
+  test.todo('Returns chat message');
+  test.todo("Doesn't respond to non-moderators");
+  test.todo('Responds to valid user-id');
+  test.todo('ERROR: no quote text provided');
+  test.todo('ERROR: no game provided when game called');
+});
+
+xdescribe('editQuote', () => {
+  test.todo('Edits quote by number');
+  test.todo('Edits quote game when called');
+  test.todo('Returns chat message');
+  test.todo("Doesn't respond to non-moderators");
+  test.todo('Responds to valid user-id');
+  test.todo('ERROR: no number provided');
+  test.todo('ERROR: quote does not exist');
+  test.todo('ERROR: no quote text provided');
+  test.todo('ERROR: no game provided when game called');
+});
+
+xdescribe('deleteQuote', () => {
+  test.todo('Deletes quote by number');
+  test.todo('Returns chat message');
+  test.todo("Doesn't respond to non-moderators");
+  test.todo('Responds to valid user-id');
+  test.todo('ERROR: no number provided');
+  test.todo('ERROR: quote does not exist');
 });

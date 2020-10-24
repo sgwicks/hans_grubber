943ff49801a6aaabe852e6254d2df60432d8f602
sgwicks <samgwicks@gmail.com>
2020-10-24 17:53:02 +0100

    Update dev command list

--- db/data/dev_data/commands.js
+++ db/data/dev_data/commands.js
@@ -1,10 +1,93 @@
 module.exports = [
   {
     command_name: 'morning',
-    command_text: 'Morning everyone',
+    command_text: 'Morning',
   },
   {
     command_name: 'danger',
     command_text: 'shanodFine Danger Heal! shanodFine',
+  },
+  {
+    command_name: 'bit',
+    command_text:
+      "If you enjoyed my Hollow Knight content then I know you'll love bit_heist - follow him over on twitch.tv/bit_heist where he streams randomisers, bingos, and speedruns.",
+  },
+  {
+    command_name: 'blog',
+    command_text:
+      'My latest blog post can be found here: https://shanodin.co.uk/how-to-support-streamers-creators/',
+  },
+  {
+    command_name: 'bye',
+    command_text: 'EarthDay Bye, have a good time zone. EarthDay',
+  },
+  { command_name: 'cat', command_text: '(=ↀωↀ=)' },
+  {
+    command_name: 'discord',
+    command_text:
+      "Come and join shanodin's stream team on discord https://discord.gg/wY54XJg we'd love to have you there.",
+  },
+  {
+    command_name: 'ellie',
+    command_text:
+      'If you love my community I bet you could find a home over with EllieJoyPanic at twitch.tv/elliejoypanic - she plays "hard and soft" games like Bloodborne and Animal Crossing.',
+  },
+  {
+    command_name: 'gfx',
+    command_text:
+      'My axolotl emote was made by LootboxGFX. I highly recommend them, their work is amazing and they were so accommodating to work with. https://twitter.com/LootboxGFX to get in touch.',
+  },
+  {
+    command_name: 'grub',
+    command_text: 'hollowGrub hollowGrub GRUB! hollowGrub hollowGrub',
+  },
+  {
+    command_name: 'humble',
+    command_text:
+      "I am a Humble Partner. If you use my partner link to buy games from the Humble store, you can support me and also the charity Shelter. Here's the link: https://www.humblebundle.com/store/?partner=shanodin",
+  },
+  {
+    command_name: 'hydrate',
+    command_text: 'SabaPing Take a sip SabaPing',
+  },
+  {
+    command_name: 'mods',
+    command_text:
+      "I'm using Living Skyrim, which is a complete overhaul including over 800 mods - https://www.wabbajack.org/#/modlists/info?machineURL=living_skyrim",
+  },
+  {
+    command_name: 'music',
+    command_text:
+      'I use StreamBeats (free music for Streamers) - https://www.youtube.com/channel/UCQ6hCusHlsIDXqoTbbTHSUQ',
+  },
+  {
+    command_name: 'psyche',
+    command_text:
+      "If you love purple and also spaaaace, I know you'll love Psyche over at twitch.tv/psyche. She's an absolute master of Elite: Dangerous, and her chat is full of hilarious people and commands (try !hot and !pls)",
+  },
+  {
+    command_name: 'resourcepack',
+    command_text:
+      'I am using PureBDcraft, which you can get here: https://bdcraft.net/downloads/purebdcraft-minecraft/',
+  },
+  {
+    command_name: 'retweet',
+    command_text:
+      'Hey - retweeting my "go live" tweet and encouraging people to come and join in is a great free way to support me. https://twitter.com/shanodin',
+  },
+  { command_name: 'safety', command_text: 'Safety kick!' },
+  {
+    command_name: 'shaders',
+    command_text:
+      "I am using Sildur's Vibrant Shaders - get them here: https://sildurs-shaders.github.io/downloads/",
+  },
+  {
+    command_name: 'stop',
+    command_text: 'shanodLol Stop reading chat! shanodLol',
+  },
+  {
+    command_name: 'water',
+    command_text:
+      'shanodLol Water is good, and you should drink some! shanodLol',
   },
 ];
